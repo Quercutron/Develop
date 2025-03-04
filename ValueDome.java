ba7a6a83788a8b95ba7b3d42b593262c4605c28b
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-04 19:09:22 +0800

    Add files via upload

---
+++ ValueDome.java
@@ -0,0 +1,11 @@
+public class ValueDome{
+	public static void main(String[] args){
+		System.out.println(123);
+		System.out.println(1.23);
+		System.out.println("abc");
+		System.out.println('a');
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println("null");
+	}
+}
