8e02b2df1e6e5bc1e3fbea54de6543fb795fe6c1
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-04 22:42:06 +0800

    Add files via upload

---
+++ VariableTest.java
@@ -0,0 +1,10 @@
+public class VariableTest {
+    public static void main(String[] args) {
+        int count=0;
+        count=count+1;
+        count=count+2-1;
+        count=count-1;
+        count=count+3;
+        System.out.println(count);
+    }
+}
