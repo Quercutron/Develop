ef4cc5f16ede0bc0d26263e26a023c896370dc29
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-04 22:18:46 +0800

    Add files via upload

--- VariableDome.java
+++ VariableDome.java
@@ -1,6 +1,9 @@
+package VariableDome;
 public class VariableDome {
     public static void main(String[] args){
         int Variable=3;
         System.out.println(Variable);
+        int a=1,b=-2;
+		System.err.println(a+b);
     }
 }
