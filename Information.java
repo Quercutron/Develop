ad402ae16c5b79ed7e40f4ebe355143f17352b07
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-05 16:28:23 +0800

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,15 @@
+public class Information {
+    //输出老师信息
+    public static void main(String[] args){
+        String name="黑马谢广坤";
+        System.out.println("姓名："+name);
+        int age=18;
+        System.out.println("年龄："+age);
+        char gender='男';
+        System.out.println("性别："+gender);
+        double heigh=180.1;
+        System.out.println("身高："+heigh);
+        char love='是';
+        System.out.println("是否单身："+love);
+    }
+}
