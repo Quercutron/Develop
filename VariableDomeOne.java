aeb0ff84d69118fc3697a6ef6330c495d7bf632f
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-05 16:12:32 +0800

    Add files via upload

---
+++ VariableDomeOne.java
@@ -0,0 +1,33 @@
+public class VariableDomeOne {
+    //数据类型分类，整数
+    public static void main(String[] args) {
+        //byte -128~127
+        byte a=10;
+        System.out.println(a);
+        //short
+        short b=20;
+        System.out.println(b);
+        //int
+        int c=30;
+        System.out.println(c);
+        //long 变量结尾要加上L
+        long d=999999L;
+        System.out.println(d);
+
+    //浮点数
+        //float 结尾要加上F；
+        float e=12.34F;
+        System.out.println(e);
+        //double
+        double f=12.5;
+        System.out.println(f);
+    //字符型
+        //char 
+        char g='中';
+        System.out.println(g);
+    //布尔型
+        //boolean
+        boolean h=false;
+        System.out.println(h);
+    }
+}
