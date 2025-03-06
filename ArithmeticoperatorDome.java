d5f39c9029645f2106296bb6210c1144e7821d57
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-06 15:13:16 +0800

    Add files via upload

---
+++ ArithmeticoperatorDome.java
@@ -0,0 +1,16 @@
+package com.itheima.Arithmeticoperator;
+
+public class ArithmeticoperatorDome {
+    //算术运算符
+    public static void main(String[] args) {
+        System.out.println(3+2);
+        System.out.println(5-2);
+        System.out.println(7*5);
+        System.out.println(9/3);
+        System.out.println(8%3);
+
+        //小数进度丢失
+        System.out.println(1.1+1.03);
+        System.out.println(1.1-1.01);
+    }
+}
