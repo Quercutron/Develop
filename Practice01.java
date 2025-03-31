4f2812548be6df2812ade22cadac84bebff0927a
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-31 19:07:20 +0800

    Add files via upload

--- Practice01.java
+++ Practice01.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Practice01 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("请问你花费了多少钱：");
+        System.out.println("请问你一共有多少钱：");
         int money=sc.nextInt();
-        if (money>100){
+        if (money>=100){
             System.out.println("网红餐厅！");
-        }else
+        }else{
             System.out.println("实惠的沙县小吃！");
+        }
     }
 }
