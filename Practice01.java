984b822e201f6b2c171eab7ef76c15c91d97111a
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-03-31 18:59:29 +0800

    Add files via upload

---
+++ Practice01.java
@@ -0,0 +1,15 @@
+package com.itheima.Practice;
+
+import java.util.Scanner;
+
+public class Practice01 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请问你花费了多少钱：");
+        int money=sc.nextInt();
+        if (money>100){
+            System.out.println("网红餐厅！");
+        }else
+            System.out.println("实惠的沙县小吃！");
+    }
+}
---
+++ Test11.java
@@ -0,0 +1,18 @@
+package com.itheima.Test;
+
+public class Test11 {
+    public static void main(String[] args) {
+        boolean isLightGreen=false;
+        boolean isLightYellow=false;
+        boolean isLightRed=true;
+        if (isLightGreen){
+            System.out.println("go!!!");
+        }
+        if (isLightYellow){
+            System.out.println("slow!");
+        }
+        if (isLightRed){
+            System.out.println("stop!");
+        }
+    }
+}
