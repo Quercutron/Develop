df50a077855b1a87884add897f128a4d4cf254b3
datapeplor <144074983+datapeplor@users.noreply.github.com>
2025-04-01 17:27:11 +0800

    Add files via upload

---
+++ Test14.java
@@ -0,0 +1,16 @@
+package com.itheima.Test;
+
+public class Test14 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            System.out.println(6-i);
+        }
+        for (int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+        for (int i=1;i<=5;i--){
+            System.out.println(i);
+        }
+
+    }
+}
