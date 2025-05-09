1d1a3b566a4f37fcf0ea835d0857c3b19216c0eb
Abraham <144074983+Quercutron@users.noreply.github.com>
2025-05-09 23:56:32 +0800

    Add files via upload

---
+++ StringTest.java
@@ -0,0 +1,14 @@
+package com.itheima.Test;
+
+public class StringTest {
+    public static void main(String[] args) {
+        //封装过的对象
+        String st="abcdefg";
+        int length=st.length();
+        System.out.println(length);
+        String big=st.toUpperCase();
+        System.out.println(big);
+    }
+
+
+}
