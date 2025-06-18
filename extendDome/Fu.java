7da0f093264caaad86e654260fbc3dc11dae8eae
Abraham <144074983+Quercutron@users.noreply.github.com>
2025-06-18 22:41:05 +0800

    Add files via upload

---
+++ extendDome/Fu.java
@@ -0,0 +1,11 @@
+package com.itheima.extendDome;
+
+public class Fu {
+    public void fuShow1(){
+        System.out.println("public---FuShow1");
+    }
+
+    private void fuShow2(){
+        System.out.println("public---FuShow2");
+    }
+}
---
+++ extendDome/TestStudent.java
@@ -0,0 +1,11 @@
+package com.itheima.extendDome;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Zi zi=new Zi();
+        System.out.println(zi);
+        zi.ziShow();
+        zi.fuShow1();
+//      zi.fuShow2();
+    }
+}
